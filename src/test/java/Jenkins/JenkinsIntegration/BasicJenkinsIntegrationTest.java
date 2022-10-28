00aedb078736d6d79fd5faa850497c2b7a7fc95d
shiva <shiva.nelikanti@gmail.com>
2022-10-28 12:34:25 +0530

    Shiva : 28th Oct 2022 - made changes for abc.java from intergration file

---
+++ src/test/java/Jenkins/JenkinsIntegration/BasicJenkinsIntegrationTest.java
@@ -0,0 +1,11 @@
+package Jenkins.JenkinsIntegration;
+
+import org.testng.annotations.Test;
+
+public class BasicJenkinsIntegrationTest {
+
+  @Test
+  public void coursesOverAllLookTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
